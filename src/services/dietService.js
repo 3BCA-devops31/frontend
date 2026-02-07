5807bf3368047ab8f89a2fdfd86f68b40e039723
kbhavani142005 <kbhavani142005@gamil.com>
2026-02-07 22:47:58 +0530

    merged backend

--- src/services/dietService.js
+++ src/services/dietService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://dietandlifestyle-backend.onrender.com/api';
 
 const dietService = {
   // Meal endpoints
